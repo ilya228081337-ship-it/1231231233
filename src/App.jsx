ccd537669e696f23ede86c2676b28b168c4fbe62
ilya228081337-ship-it <ilya228081337@gmail.com>
2026-01-16 15:07:38 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>Repair Control</h1>
+        <p>
+          Добро пожаловать в систему контроля ремонта! Здесь вы можете отслеживать
+          статус выполнения работ и получать актуальную информацию о ходе ремонта.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
